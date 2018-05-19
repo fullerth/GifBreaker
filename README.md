c007569af94de996f210e6e1b95e56c2d1442637
Thomas Fuller <thomas@mcfuller.com>
2018-05-19 14:45:56 -0400

    Add a gif_element interface class

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # GifBreaker
 A Python representation of a gif file with knobs on as many settings as possible
 
+# Requirements
+Must be run with Python 3+ due to the way this represents and processes hex data 
+as strings of hex characters.
+
 # Acknowledgements
 http://www.matthewflickinger.com/lab/whatsinagif/bits_and_bytes.asp
--- src/GifBreaker.py
+++ src/GifBreaker.py
@@ -1,21 +1,29 @@
+class gif_element():
+    # Equivalent to "GIF89a" version
+    def __init__(self, element):
+        self.element = element
+    def get_element(self):
+        return self.element
+
 class GifBreaker():
-    header = '474946383961'
-    logical_screen_descriptor = '0A000A00910000'
-    global_color_table = 'FFFFFFFF00000000FF000000'
-    graphics_control_extension = '21F9040000000000'
-    image_descriptor = '2C000000000A000A0000'
-    image_data = '02168C2D99872A1CDC33A00275EC95FAA8DE608C04914C0100'
-    trailer = '3B'
+    def __init__(self):
+        self.header = gif_element('474946383961')
+        self.logical_screen_descriptor = gif_element('0A000A00910000')
+        self.global_color_table = gif_element('FFFFFFFF00000000FF000000')
+        self.graphics_control_extension = gif_element('21F9040000000000')
+        self.image_descriptor = gif_element('2C000000000A000A0000')
+        self.image_data = gif_element('02168C2D99872A1CDC33A00275EC95FAA8DE608C04914C0100')
+        self.trailer = gif_element('3B')
 
     def write_gif_to_file(self):
         with open("output.gif", "wb") as f:
-            f.write(bytes.fromhex(self.header))
-            f.write(bytes.fromhex(self.logical_screen_descriptor))
-            f.write(bytes.fromhex(self.global_color_table))
-            f.write(bytes.fromhex(self.graphics_control_extension))
-            f.write(bytes.fromhex(self.image_descriptor))
-            f.write(bytes.fromhex(self.image_data))
-            f.write(bytes.fromhex(self.trailer))
+            f.write(bytes.fromhex(self.header.get_element()))
+            f.write(bytes.fromhex(self.logical_screen_descriptor.get_element()))
+            f.write(bytes.fromhex(self.global_color_table.get_element()))
+            f.write(bytes.fromhex(self.graphics_control_extension.get_element()))
+            f.write(bytes.fromhex(self.image_descriptor.get_element()))
+            f.write(bytes.fromhex(self.image_data.get_element()))
+            f.write(bytes.fromhex(self.trailer.get_element()))
     
 if __name__=="__main__":
     gb = GifBreaker()
